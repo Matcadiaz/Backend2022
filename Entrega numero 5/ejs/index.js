ac62646c23ca3242c24e03b48511c69451549453
Matias Diaz <Matiascdiaz@gmail.com>
2022-12-16 01:09:02 -0300

    Desafio terminado en ejs

---
+++ Entrega numero 5/ejs/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("public"))
+
+let productos = [];
+
+app.get("/", (req, res) => {
+  res.render('pages/formulario', {productos, title: "productos"});
+});
+
+app.post("/productos", (req, res)=>{
+
+  productos.push(req.body);
+
+  res.redirect('/');
+})
+
+app.get('/productos', (req, res)=>{
+  res.render('pages/tablaHtml', {productos, title: "productos"})
+})
+
+const PORT = 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port ${server.address().port}`);
+});
+
+server.on("error", (error) =>
+  console.log(`An error ocurred on server ${error.message}`)
+);
